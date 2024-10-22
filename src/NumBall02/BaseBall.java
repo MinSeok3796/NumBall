c237f3bb1a482c967eec1d6fafabfca6fb6efefe
MinSeok3796 <ms3796@naver.com>
2024-10-22 14:54:42 +0900

    Lv.2구현, 게임 시작시 옵션 선택기능 추가(Lv1의 코드를 바탕으로 구현 )

---
+++ src/NumBall02/BaseBall.java
@@ -0,0 +1,47 @@
+package NumBall02;
+
+public class BaseBall {
+    private int answer;
+    private String answerString;
+
+    // 생성자에 의해 생성될때 랜덤함수를 이용하여 정답 생성
+    public BaseBall() {
+        int hundred = (int) (Math.random() * 9) + 1;
+        int ten;
+        int one;
+        do {
+            ten = (int) (Math.random() * 9) + 1;
+        } while (ten == hundred);
+
+        do {
+            one = (int) (Math.random() * 9) + 1;
+        } while (one == hundred || one == ten);
+        this.answer = hundred * 100 + ten * 10 + one;
+        System.out.println(answer);
+    }
+    //게임의 진행 부분
+    public boolean play(int num){
+        String mynum = Integer.toString(num);
+        answerString = Integer.toString(this.answer);
+        int strike = 0;
+        int ball = 0;
+        for(int a = 0; a < 3; a++){
+            for(int b = 0; b < 3; b++){
+                if( a == b && mynum.charAt(a) == answerString.charAt(b)){
+                    strike++;
+                }else if( a != b && mynum.charAt(a) == answerString.charAt(b)){
+                    ball++;
+                }
+            }
+        }
+        if(strike == 3){
+            System.out.println("정답입니다.");
+        }else if(strike == 0 && ball == 0) {
+            System.out.println("아웃입니다.");
+        }else{
+            System.out.println(strike + "스트라이크 " + ball + "볼입니다.");
+        }
+        return num != answer;
+    }
+
+}
---
+++ src/NumBall02/Main.java
@@ -0,0 +1,73 @@
+package NumBall02;
+
+import NumBall01.BaseBall;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        // 값 입력을 위한 스캐너 객체 생성
+        Scanner sc = new Scanner(System.in);
+        BaseBall baseBall = new BaseBall();  //야구 게임 생성
+        boolean play = true;
+        int mynum = 0;
+        int playnum = 0;
+        do {
+
+            System.out.println("환영합니다! 원하시는 번호를 입력해주세요.");
+            System.out.println("1. 게임 시작하기  2. 게임 기록 보기  3. 종료하기");
+            playnum = sc.nextInt();
+            switch (playnum) {
+                case 1:
+                    System.out.println(" < 게임을 시작합니다 >");
+
+                    do {
+                        try {
+                            HashSet<Integer> hashSet = new HashSet<>(); // 입력값 체크
+                            System.out.println("숫자를 입력하세요");
+                            mynum = sc.nextInt();
+
+                            if (mynum < 100 || mynum > 999) { //입력값이 3자리인지 확인
+                                System.out.println("입력하신 숫자가 3자리인지 확인해주세요");
+                                sc.nextLine();
+                                continue;
+                            } else {
+                                // 각 자리수를 HashSet에 저장
+                                hashSet.add(mynum / 100);
+                                hashSet.add((mynum % 100) / 10);
+                                hashSet.add((mynum % 100) % 10);
+
+                                if (hashSet.contains(0)) { //입력값에 0이 있는경우
+                                    System.out.println("입력하신 숫자중에 0이 있습니다. 다시입력해주세요");
+                                    sc.nextLine();
+                                    continue;
+                                } else if (hashSet.size() != 3) { //입력값에 중복이 있는경우
+                                    System.out.println("입력하신 숫자중에 중복이 있습니다. 다시입력해주세요");
+                                    sc.nextLine();
+                                    continue;
+                                }
+                            }
+                        } catch (Exception e) {
+                            System.out.println("올바르지 못한 입력입니다.");
+                            sc.nextLine();
+                            continue;
+                        }
+                        play = baseBall.play(mynum);
+                    } while (play);
+                    break;
+                case 2:
+                    System.out.println("Lv.3에서 구현 예정입니다. 조금만 기다려주세요");
+                    break;
+                case 3:
+                    System.out.println("종료합니다.");
+                    return;
+                default:
+                    System.out.println("1,2,3번중에 하나로 골라주세요");
+                    break;
+            }
+            play = true;
+        }while(play);
+
+    }
+}
