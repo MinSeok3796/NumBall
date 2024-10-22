b764c0ced03d98b7e09a96590ab410a52ebcd62c
MinSeok3796 <ms3796@naver.com>
2024-10-22 15:23:00 +0900

    3단계 기능 구현(게임 기록통계, 출력개선), count변수를 이용하여 이용횟수 저장

--- src/NumBall03/Main.java
+++ src/NumBall03/Main.java
@@ -1,4 +1,4 @@
-package NumBall02;
+package NumBall03;
 
 import java.util.*;
 
@@ -6,25 +6,25 @@ public class Main {
     public static void main(String[] args) throws Exception{
         // 값 입력을 위한 스캐너 객체 생성
         Scanner sc = new Scanner(System.in);
-        BaseBall baseBall = new BaseBall();  //야구 게임 생성
         boolean play = true;
         int mynum = 0;
         int playnum = 0;
-        do {
 
-            System.out.println("환영합니다! 원하시는 번호를 입력해주세요.");
+        ArrayList<Integer> tryarr = new ArrayList<Integer>();
+        System.out.println("환영합니다! 원하시는 번호를 입력해주세요.");
+        do {
             System.out.println("1. 게임 시작하기  2. 게임 기록 보기  3. 종료하기");
             playnum = sc.nextInt();
             switch (playnum) {
                 case 1:
+                    BaseBall baseBall = new BaseBall();  //야구 게임 생성
                     System.out.println(" < 게임을 시작합니다 >");
-
+                    int count = 0;
                     do {
                         try {
                             HashSet<Integer> hashSet = new HashSet<>(); // 입력값 체크
                             System.out.println("숫자를 입력하세요");
                             mynum = sc.nextInt();
-
                             if (mynum < 100 || mynum > 999) { //입력값이 3자리인지 확인
                                 System.out.println("입력하신 숫자가 3자리인지 확인해주세요");
                                 sc.nextLine();
@@ -51,10 +51,18 @@ public class Main {
                             continue;
                         }
                         play = baseBall.play(mynum);
+                        count++;
                     } while (play);
+                    tryarr.add(count);
                     break;
                 case 2:
-                    System.out.println("Lv.3에서 구현 예정입니다. 조금만 기다려주세요");
+                    if(tryarr.size() == 0) {
+                        System.out.println("게임을 실행한 기록이 없습니다.");
+                    }else {
+                        for (int i = 0; i < tryarr.size(); i++) {
+                            System.out.println(i + 1 + "번째 게임 : 시도횟수 - " + tryarr.get(i));
+                        }
+                    }
                     break;
                 case 3:
                     System.out.println("종료합니다.");
