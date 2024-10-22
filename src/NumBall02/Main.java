eda182d2f29b2059ffd9c6d701e320d08e6e7bbc
MinSeok3796 <ms3796@naver.com>
2024-10-22 15:30:57 +0900

    2단계 옵션 선택 수정

--- src/NumBall02/Main.java
+++ src/NumBall02/Main.java
@@ -11,7 +11,6 @@ public class Main {
         int mynum = 0;
         int playnum = 0;
         do {
-
             System.out.println("환영합니다! 원하시는 번호를 입력해주세요.");
             System.out.println("1. 게임 시작하기  2. 게임 기록 보기  3. 종료하기");
             playnum = sc.nextInt();
@@ -60,7 +59,6 @@ public class Main {
                     System.out.println("종료합니다.");
                     return;
                 default:
-                    System.out.println("1,2,3번중에 하나로 골라주세요");
                     break;
             }
             play = true;
